0db00ad61f766d24c815cd407ccf782219bb6ebb
phaishuk <xomas@ukr.net>
2023-07-11 00:16:14 +0300

    create urls in payment app

---
+++ payment/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+
+from payment.views import PaymentViewSet
+
+app_name = "payment"
+
+urlpatterns = [
+    path(
+        "",
+        PaymentViewSet.as_view(actions={"get": "list"}),
+        name="payment-list",
+    ),
+    path(
+        "<int:pk>/",
+        PaymentViewSet.as_view(actions={"get": "retrieve"}),
+        name="payment-detail",
+    ),
+    path(
+        "success/",
+        PaymentViewSet.as_view(actions={"get": "success"}),
+        name="payment-success",
+    ),
+    path(
+        "cancel/",
+        PaymentViewSet.as_view(actions={"get": "cancel"}),
+        name="payment-cancel",
+    ),
+]
