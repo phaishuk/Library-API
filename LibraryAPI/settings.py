4ac24c0712a6428f5b4c3760fc0e3d0f1b9431d5
phaishuk <xomas@ukr.net>
2023-07-12 19:59:14 +0300

    configure docker, and postgres db settings.py

--- LibraryAPI/settings.py
+++ LibraryAPI/settings.py
@@ -103,8 +103,11 @@ WSGI_APPLICATION = "LibraryAPI.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
     }
 }
 
