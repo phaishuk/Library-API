02fd445db2ba27cab3debaa053fe2b017f687e63
phaishuk <xomas@ukr.net>
2023-08-22 19:49:05 +0300

    rm redundant commented settings

--- LibraryAPI/settings.py
+++ LibraryAPI/settings.py
@@ -102,25 +102,15 @@ WSGI_APPLICATION = "LibraryAPI.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("POSTGRES_DB"),
-#         "USER": os.getenv("POSTGRES_USER"),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-#         "HOST": os.getenv("POSTGRES_HOST"),
-#     }
-# }
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "library",
-        "USER": "username",
-        "PASSWORD": "1234qwer!@#$",
-        "HOST": "localhost",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
     }
-}  # for local testing
+}
 
 
 # Password validation
